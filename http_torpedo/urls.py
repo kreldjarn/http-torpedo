6d1b056841ededd39516693360e46f98d70c351b
Kristján Eldjárn <kristjan@eldjarn.net>
2015-08-11 12:36:19 +0000

    Switched absolute urls out for generated ones

--- http_torpedo/urls.py
+++ http_torpedo/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^account/login/$', login.as_view()),
-    url(r'^account/authenticate/$', auth_view),
-    url(r'^account/logout/$', logout.as_view()),
+    url(r'^account/login/$', login.as_view(), name='login'),
+    url(r'^account/authenticate/$', auth_view, name='authenticate'),
+    url(r'^account/logout/$', logout.as_view(), name='logout'),
     url(r'^account/logged/$', logged.as_view()),
     url(r'^account/invalid/$', invalid.as_view()),
 
-    url(r'^account/register/$', register.as_view()),
+    url(r'^account/register/$', register.as_view(), name='register'),
     url(r'^account/success/$', success.as_view()),
 ]
