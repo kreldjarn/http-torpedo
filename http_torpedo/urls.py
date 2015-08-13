2481f08b0f9ab49817d395d26e86450bcddb238c
Hrafn Eiríksson <hrafne@gmail.com>
2015-08-13 12:39:26 +0000

    Some refactorization that @kreldjarn wanted me to do.

--- http_torpedo/urls.py
+++ http_torpedo/urls.py
@@ -1,29 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from torpedo.views import *
-from django.http import HttpResponse
-
-from torpedo.models import TestRun
-
-import json
-import requests
-
-SERVERS = [
-	'http://noc2.srv.oz.com:6666'
-];
-
-def run_test(args):
-	server = SERVERS[0] # TODO: this.
-	payload = { 'file': open('/tmp/test.txt', 'rb') }
-	data = { 'calls': '1', 'conns': '1', 'domain': 'www.mbl.is' }
-	r = requests.post(server, data=data, files=payload, timeout=10000)
-	stuff = json.loads(r.content)
-
-	x = TestRun()
-	x.json = stuff
-	x.save()
-
-	return HttpResponse(r.content)
 
 urlpatterns = [
 	# LOGIC
